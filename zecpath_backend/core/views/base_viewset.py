feb2944ef7b300dc07c240d59c8019632dcbb83c
Kalkin369 <prakashvishnu749@gmail.com>
2026-04-19 15:06:00 +0530

    edit baseviewset

--- zecpath_backend/core/views/base_viewset.py
+++ zecpath_backend/core/views/base_viewset.py
@@ -19,6 +19,10 @@ class BaseViewSet(ModelViewSet):
         response = super().update(request,*args,**kwargs)
         return success_response(response.data,message="Updated successfully")
     
+    def partial_update(self, request, *args, **kwargs):
+        response =super().partial_update(request, *args, **kwargs)
+        return success_response(response.data,message="Updated successfully")
+    
     def destroy(self, request, *args, **kwargs):
         super().destroy(request, *args, **kwargs)
         return success_response(message="Deleted successfully")

305682f8a0facb3bfbaa1aecb82ea4e602ff56e0
Kalkin369 <prakashvishnu749@gmail.com>
2026-04-26 11:42:01 +0530

    Uncommented setting in settings.py

--- zecpath_backend/zecpath_backend/settings.py
+++ zecpath_backend/zecpath_backend/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 
-    # 'EXCEPTION_HANDLER':'core.utils.exception_handler.custom_exception_handler',
+    'EXCEPTION_HANDLER':'core.utils.exception_handler.custom_exception_handler',
 
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination','PAGE_SIZE':5,
 

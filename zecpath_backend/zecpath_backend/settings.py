453c6fef5f7bfa54031738ae3022c1fa8c7a211e
Kalkin369 <prakashvishnu749@gmail.com>
2026-07-30 22:07:15 +0530

    Add STATIC_ROOT for production deployment

--- zecpath_backend/zecpath_backend/settings.py
+++ zecpath_backend/zecpath_backend/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Tell Django to use this model
 
